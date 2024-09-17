5170b29dc66cdff14f25417e0683975740b4b533
Dilnaz Nauryzbayeva <dilnaz.nauryzbayeva@nu.edu.kz>
2024-09-17 20:00:18 +0500

    implement sign-up functionality - not finished

--- FMA_mob/View/Login/SignUpView.swift
+++ FMA_mob/View/Login/SignUpView.swift
@@ -7,9 +7,138 @@
 
 import SwiftUI
 
+extension View {
+    func disableWithCustomOpacity(_ condition: Bool) -> some View {
+        self
+            .disabled(condition)
+            .opacity(condition ? 0.5 : 1) // Adjust the opacity based on the condition
+    }
+}
+
 struct SignUpView: View {
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    @StateObject var mainVM = MainViewModel.shared
+    
+    private var isFormValid: Bool {
+        // Check if all fields are filled
+        !(mainVM.txtUsername.isEmpty || mainVM.txtEmail.isEmpty || mainVM.txtPassword.isEmpty)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            
+            Image("bottom_bg")
+                .resizable()
+                .scaledToFill()
+                .frame(width: .screenWidth, height: .screenHeight)
+            
+            ScrollView {
+                VStack {
+                    
+                    Image("LogoLogo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 40)
+                    
+                    Text("Get Started")
+                        .font(.customfont(.bold, fontSize: 30))
+                        .foregroundColor(.primaryText)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        .padding(.bottom, 2)
+                        .padding(.top, 15)
+                    
+                    Text("by creating a free account")
+                        .font(.customfont(.regular, fontSize: 16))
+                        .foregroundColor(.secondaryText)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        .padding(.bottom, .screenWidth * 0.1)
+                    
+                    LineTextField(title: "Your Name", placholder: "Enter your username", txt: $mainVM.txtUsername)
+                        .padding(.bottom, .screenWidth * 0.07)
+                    
+                    LineTextField(title: "Your Email", placholder: "Enter your email address", txt: $mainVM.txtEmail, keyboardType: .emailAddress)
+                        .padding(.bottom, .screenWidth * 0.07)
+                    
+                    LineSecureField(title: "Password", placholder: "Enter your password", txt: $mainVM.txtPassword, isShowPassword: $mainVM.isShowPassword)
+                        .padding(.bottom, .screenWidth * 0.04)
+                    
+                    VStack {
+                        Text("By continuing you agree to our")
+                            .font(.customfont(.regular, fontSize: 14))
+                            .foregroundColor(.secondaryText)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        
+                        HStack {
+                            Text("Terms of Service")
+                                .font(.customfont(.regular, fontSize: 14))
+                                .foregroundColor(.primaryApp)
+                            
+                            Text("and")
+                                .font(.customfont(.regular, fontSize: 14))
+                                .foregroundColor(.secondaryText)
+                            
+                            Text("Privacy Policy.")
+                                .font(.customfont(.regular, fontSize: 14))
+                                .foregroundColor(.primaryApp)
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        }
+                        .padding(.bottom, .screenWidth * 0.02)
+                    }
+                    
+                    RoundButton(title: "Next") {
+                        mainVM.serviceCallSignUp()
+                    }
+                    .padding(.bottom, .screenWidth * 0.05)
+                    .disableWithCustomOpacity(!isFormValid) // Disable if the form is not valid
+                    
+                    NavigationLink {
+                        LoginView()
+                    } label: {
+                        HStack {
+                            Text("Already have an account?")
+                                .font(.customfont(.regular, fontSize: 14))
+                                .foregroundColor(.primaryText)
+                            
+                            Text("Sign In")
+                                .font(.customfont(.regular, fontSize: 14))
+                                .foregroundColor(.primaryApp)
+                        }
+                    }
+                    
+                    Spacer()
+                }
+                .padding(.top, .topInsets + 64)
+                .padding(.horizontal, 20)
+                .padding(.bottom, .bottomInsets)
+            }
+            
+            VStack {
+                HStack {
+                    Button {
+                        mode.wrappedValue.dismiss()
+                    } label: {
+                        Image("back")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 25, height: 25)
+                    }
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+            }
+            .padding(.top, .topInsets)
+            .padding(.horizontal, 20)
+        }
+        .alert(isPresented: $mainVM.showError) {
+            Alert(title: Text(Globs.AppName), message: Text(mainVM.errorMessage), dismissButton: .default(Text("Ok")))
+        }
+        .navigationTitle("")
+        .navigationBarHidden(true)
+        .navigationBarBackButtonHidden(true)
+        .ignoresSafeArea()
     }
 }
 
@@ -18,3 +147,4 @@ struct SignUpView_Previews: PreviewProvider {
         SignUpView()
     }
 }
+
